fa8a440b4c1ea36dc81cbb67b2d5073aa0f1e156
harlemnight <33836858@qq.com>
2020-05-28 18:04:22 +0800

    11

--- example/eg_read_xsl_todb.py
+++ example/eg_read_xsl_todb.py
@@ -2,14 +2,15 @@ import configparser
 import cx_Oracle as orac
 import pandas as pd
 import os
+import xlrd
 #py2.7.14old server may be utf-8' codec can't decode byte 0x8b in position 2:
 #localmachine is no problem
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
 
 def main(filepath):
-    os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
     cf = configparser.ConfigParser()
-    cf.read(filepath)
+    cf.read(filepath, encoding='utf-8-sig')
     db_cfg = cf.items(section='db_config')
     db_dict = {i[0]: i[1] for i in db_cfg}
     user = db_dict['user']
@@ -17,9 +18,11 @@ def main(filepath):
     connect = db_dict['connect']
     tablename = cf.get(section='sql', option='table_name')
     file_name = cf.get(section='file', option='file_name')
-    df = pd.read_excel(file_name)
-    data = df.head()  # 默认读取前5行的数据
-    print("获取到所有的值:\n{0}".format(data))  # 格式化输出
+    #content = xlrd.open_workbook(filename=file_name, encoding_override='gbk')
+    df = pd.read_excel(file_name, nrows=1)
+    #data = df.head()  # 默认读取前5行的数据
+    print(len(df))
+    #print("获取到所有的值:\n{0}".format(data))  # 格式化输出
     # try:
     #     dbcon = orac.connect(user, password, connect)
     #     #do somthing
@@ -28,3 +31,5 @@ def main(filepath):
     # finally:
     #     dbcon.close()
 
+
+main('config.ini')

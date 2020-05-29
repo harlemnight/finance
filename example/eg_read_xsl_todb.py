e5ace63adf3f2892c5a532ffde527059eec65a54
harlemnight <33836858@qq.com>
2020-05-29 17:52:04 +0800

    123

--- example/eg_read_xsl_todb.py
+++ example/eg_read_xsl_todb.py
@@ -10,26 +10,33 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
 def main(filepath):
     cf = configparser.ConfigParser()
-    cf.read(filepath, encoding='utf-8-sig')
+    cf.read(filepath, encoding='utf-8-sig') #chinese
     db_cfg = cf.items(section='db_config')
     db_dict = {i[0]: i[1] for i in db_cfg}
     user = db_dict['user']
     password = db_dict['pwd']
     connect = db_dict['connect']
+    commit_row = db_dict['commit_row']
     tablename = cf.get(section='sql', option='table_name')
     file_name = cf.get(section='file', option='file_name')
     #content = xlrd.open_workbook(filename=file_name, encoding_override='gbk')
-    df = pd.read_excel(file_name, nrows=1)
+    df = pd.read_excel(file_name)
     #data = df.head()  # 默认读取前5行的数据
-    print(len(df))
-    #print("获取到所有的值:\n{0}".format(data))  # 格式化输出
-    # try:
-    #     dbcon = orac.connect(user, password, connect)
-    #     #do somthing
-    # except Exception as e:
-    #     dbcon.rollback()
-    # finally:
-    #     dbcon.close()
+    try:
+        dbcon = orac.connect(user, password, connect)
+        rs = dbcon.cursor()
+        sql = 'insert into '.table_name;
+        for i in range(len(df)):
+            rs.execute(sql % (df.ix[i, 0], df.ix[i, 1], df.ix[i, 2], df.ix[i, 3]))
+        if i % 10000 == 0:
+            dbcon.commit()
+
+    except Exception as e:
+        dbcon.rollback()
+    finally:
+        dbcon.commit()
+        rs.close()
+        dbcon.close()
 
 
 main('config.ini')
